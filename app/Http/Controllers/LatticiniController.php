f6689fbfc332ae08783c5b2f595498d043abd9de
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-03-23 17:41:42 +0100

    sistemati ";" mancanti

--- app/Http/Controllers/LatticiniController.php
+++ app/Http/Controllers/LatticiniController.php
@@ -8,23 +8,23 @@ class LatticiniController extends Controller
 {
   public function ricottaIndex()
   {
-    return view('latticini.ricotta')
+    return view('latticini.ricotta');
   }
   public function formaggioIndex()
   {
-    return view('latticini.formaggio')
+    return view('latticini.formaggio');
   }
   public function pannaIndex()
   {
-    return view('latticini.panna')
+    return view('latticini.panna');
   }
   public function yogurtIndex()
   {
-    return view('latticini.yogurt')
+    return view('latticini.yogurt');
   }
   public function burroIndex()
   {
-    return view('latticini.burro')
+    return view('latticini.burro');
   }
   public function nuovoProdotto(Request $request)
   {
--- resources/views/includes/body/_header.blade.php
+++ resources/views/includes/body/_header.blade.php
@@ -3,11 +3,11 @@
     <a class="header__navbar__home" href="{{ route('latticini.home') }}">
       Latticini
     </a>
-    <a class="header__navbar__link" href="{{ rounte('latticini.formaggio') }}">Formaggio</a>
-    <a class="header__navbar__link" href="{{ rounte('latticini.ricotta') }}">Ricotta</a>
-    <a class="header__navbar__link" href="{{ rounte('latticini.burro') }}">Burro</a>
-    <a class="header__navbar__link" href="{{ rounte('latticini.yogurt') }}">Yogurt</a>
-    <a class="header__navbar__link" href="{{ rounte('latticini.panna') }}">Panna</a>
-    <a class="header__navbar__link" href="{{ rounte('latticini.nuovo_prodotto') }}">Nuovo Prodotto</a>
+    <a class="header__navbar__link" href="{{ route('latticini.formaggio') }}">Formaggio</a>
+    <a class="header__navbar__link" href="{{ route('latticini.ricotta') }}">Ricotta</a>
+    <a class="header__navbar__link" href="{{ route('latticini.burro') }}">Burro</a>
+    <a class="header__navbar__link" href="{{ route('latticini.yogurt') }}">Yogurt</a>
+    <a class="header__navbar__link" href="{{ route('latticini.panna') }}">Panna</a>
+    <a class="header__navbar__link" href="{{ route('latticini.nuovo_prodotto') }}">Nuovo Prodotto</a>
   </nav>
 </header>
