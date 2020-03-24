bcbb4d0123b948caba2604aa28badfd998fb94fc
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-03-24 15:43:08 +0100

    aggiunto ciclo nell'_header per poter prendere dinamicamente i links

--- public/js/app.js
+++ public/js/app.js
@@ -19335,8 +19335,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! F:\alessandro\boolean.careers\Esercitazioni\mamp_public\laravel-base-crud\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! F:\alessandro\boolean.careers\Esercitazioni\mamp_public\laravel-base-crud\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! F:\alessandro\Boolean.careers\Esercitazioni\mamp_public\laravel-base-crud\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! F:\alessandro\Boolean.careers\Esercitazioni\mamp_public\laravel-base-crud\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/includes/body/_header.blade.php
+++ resources/views/includes/body/_header.blade.php
@@ -1,13 +1,15 @@
+@php
+  use App\Latticino;
+  $latticini = Latticino::all();
+@endphp
 <header class="header">
   <nav class="header__navbar">
     <a class="header__navbar__home" href="{{ route('latticini.home') }}">
       Latticini
     </a>
-    <a class="header__navbar__link" href="{{ route('latticini.formaggio') }}">Formaggio</a>
-    <a class="header__navbar__link" href="{{ route('latticini.ricotta') }}">Ricotta</a>
-    <a class="header__navbar__link" href="{{ route('latticini.burro') }}">Burro</a>
-    <a class="header__navbar__link" href="{{ route('latticini.yogurt') }}">Yogurt</a>
-    <a class="header__navbar__link" href="{{ route('latticini.panna') }}">Panna</a>
+    @foreach ($latticini as $latticino)
+      <a class="header__navbar__link" href="{{ route('latticini.'.$latticino['prodotto']) }}">{{$latticino['prodotto']}}</a>
+    @endforeach
     <a class="header__navbar__link" href="{{ route('latticini.nuovo_prodotto') }}">Nuovo Prodotto</a>
   </nav>
 </header>
